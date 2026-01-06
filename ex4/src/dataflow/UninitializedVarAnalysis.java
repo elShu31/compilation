b8975164e6f88166ff0db78a4a5253dff19d9ffa
Maxim German <maximgerman1@mail.tau.ac.il>
2026-01-06 15:10:43 +0200

    Fixes (#33)
    * Fixes
    
    * Externals
    
    ---------
    
    Co-authored-by: Maxim <maximg@remitly.com>

--- ex4/src/dataflow/UninitializedVarAnalysis.java
+++ ex4/src/dataflow/UninitializedVarAnalysis.java
@@ -80,35 +80,29 @@ public class UninitializedVarAnalysis extends DataflowAnalysis<UninitializedVarS
     /**
      * Create the boundary state for the entry node.
      * Scan global variable declarations (before main) to find uninitialized globals.
+     * Also tracks tainted temps to properly handle initialization chains.
      */
     @Override
     protected UninitializedVarState createBoundaryState()
     {
         UninitializedVarState state = new UninitializedVarState();
-        
+
         // Process all commands before main to determine initial global state
-        // We simulate the effect of global declarations
+        // We simulate the effect of global declarations with taint tracking
         List<IrCommand> commands = cfg.getCommands();
         for (int i = 0; i < mainLabelIndex; i++)
         {
             IrCommand cmd = commands.get(i);
-            if (cmd instanceof IrCommandAllocate)
-            {
-                // Global variable declared - starts uninitialized
-                state.addUninitialized(((IrCommandAllocate) cmd).varId);
-            }
-            else if (cmd instanceof IrCommandStore)
-            {
-                // Global variable initialized
-                state.removeUninitialized(((IrCommandStore) cmd).varId);
-            }
+            // Reuse the transfer function to handle all command types consistently
+            transfer(cmd, state);
         }
-        
+
         return state;
     }
     
     /**
      * Apply the transfer function for a single IR command.
+     * Tracks both uninitialized variables and tainted temporaries.
      */
     @Override
     protected void transfer(IrCommand cmd, UninitializedVarState state)
@@ -119,23 +113,95 @@ public class UninitializedVarAnalysis extends DataflowAnalysis<UninitializedVarS
             VarId varId = ((IrCommandAllocate) cmd).varId;
             state.addUninitialized(varId);
         }
-        else if (cmd instanceof IrCommandStore)
-        {
-            // kill: variable becomes initialized
-            VarId varId = ((IrCommandStore) cmd).varId;
-            state.removeUninitialized(varId);
-        }
         else if (cmd instanceof IrCommandLoad)
         {
-            // Check if loading an uninitialized variable
-            VarId varId = ((IrCommandLoad) cmd).varId;
-            if (state.isUninitialized(varId))
+            // Load dst <- var
+            IrCommandLoad load = (IrCommandLoad) cmd;
+            if (state.isUninitialized(load.varId))
             {
-                // Record the variable name (not the full VarId)
-                uninitializedUses.add(varId.name);
+                // Report the uninitialized use
+                uninitializedUses.add(load.varId.name);
+                // Taint the destination temp
+                state.addTainted(load.dst);
+            }
+            else
+            {
+                // Variable is initialized, so temp is clean
+                state.removeTainted(load.dst);
             }
         }
-        // All other commands: no effect on uninitialized state
+        else if (cmd instanceof IrCommandStore)
+        {
+            // Store var <- src
+            IrCommandStore store = (IrCommandStore) cmd;
+            if (state.isTainted(store.src))
+            {
+                // Storing a tainted value - variable remains/becomes uninitialized
+                state.addUninitialized(store.varId);
+            }
+            else
+            {
+                // Storing an initialized value - variable becomes initialized
+                state.removeUninitialized(store.varId);
+            }
+        }
+        else if (cmd instanceof IRcommandConstInt)
+        {
+            // ConstInt dst <- value: constants are always initialized
+            IRcommandConstInt constInt = (IRcommandConstInt) cmd;
+            state.removeTainted(constInt.t);
+        }
+        else if (cmd instanceof IrCommandBinopAddIntegers)
+        {
+            IrCommandBinopAddIntegers binop = (IrCommandBinopAddIntegers) cmd;
+            transferBinop(state, binop.dst, binop.t1, binop.t2);
+        }
+        else if (cmd instanceof IrCommandBinopSubIntegers)
+        {
+            IrCommandBinopSubIntegers binop = (IrCommandBinopSubIntegers) cmd;
+            transferBinop(state, binop.dst, binop.t1, binop.t2);
+        }
+        else if (cmd instanceof IrCommandBinopMulIntegers)
+        {
+            IrCommandBinopMulIntegers binop = (IrCommandBinopMulIntegers) cmd;
+            transferBinop(state, binop.dst, binop.t1, binop.t2);
+        }
+        else if (cmd instanceof IrCommandBinopDivIntegers)
+        {
+            IrCommandBinopDivIntegers binop = (IrCommandBinopDivIntegers) cmd;
+            transferBinop(state, binop.dst, binop.t1, binop.t2);
+        }
+        else if (cmd instanceof IrCommandBinopLtIntegers)
+        {
+            IrCommandBinopLtIntegers binop = (IrCommandBinopLtIntegers) cmd;
+            transferBinop(state, binop.dst, binop.t1, binop.t2);
+        }
+        else if (cmd instanceof IrCommandBinopGtIntegers)
+        {
+            IrCommandBinopGtIntegers binop = (IrCommandBinopGtIntegers) cmd;
+            transferBinop(state, binop.dst, binop.t1, binop.t2);
+        }
+        else if (cmd instanceof IrCommandBinopEqIntegers)
+        {
+            IrCommandBinopEqIntegers binop = (IrCommandBinopEqIntegers) cmd;
+            transferBinop(state, binop.dst, binop.t1, binop.t2);
+        }
+        // Labels, jumps, PrintInt: no effect on uninitialized/tainted state
+    }
+
+    /**
+     * Helper for binary operations: if either operand is tainted, result is tainted.
+     */
+    private void transferBinop(UninitializedVarState state, temp.Temp dst, temp.Temp t1, temp.Temp t2)
+    {
+        if (state.isTainted(t1) || state.isTainted(t2))
+        {
+            state.addTainted(dst);
+        }
+        else
+        {
+            state.removeTainted(dst);
+        }
     }
     
     /****************************************/
--- ex4/src/dataflow/UninitializedVarState.java
+++ ex4/src/dataflow/UninitializedVarState.java
@@ -12,11 +12,13 @@ import java.util.*;
 /* PROJECT IMPORTS */
 /*******************/
 import ir.VarId;
+import temp.Temp;
 
 /**
  * Dataflow state for "may be uninitialized" analysis.
- * 
+ *
  * Tracks a set of variables that may be uninitialized at a given program point.
+ * Also tracks temporaries that are "tainted" (hold values from uninitialized variables).
  * This is a "may" analysis, so join is set union.
  */
 public class UninitializedVarState implements DataflowState<UninitializedVarState>
@@ -24,24 +26,29 @@ public class UninitializedVarState implements DataflowState<UninitializedVarStat
     /****************/
     /* DATA MEMBERS */
     /****************/
-    
+
     /** Set of variables that may be uninitialized */
     private Set<VarId> uninitializedVars;
-    
+
+    /** Set of temporaries that hold values from uninitialized sources */
+    private Set<Integer> taintedTemps;
+
     /******************/
     /* CONSTRUCTOR(S) */
     /******************/
-    
+
     /** Create an empty state (bottom element) */
     public UninitializedVarState()
     {
         this.uninitializedVars = new HashSet<>();
+        this.taintedTemps = new HashSet<>();
     }
-    
-    /** Create a state with the given set of uninitialized variables */
-    public UninitializedVarState(Set<VarId> uninitializedVars)
+
+    /** Create a state with the given sets */
+    public UninitializedVarState(Set<VarId> uninitializedVars, Set<Integer> taintedTemps)
     {
         this.uninitializedVars = new HashSet<>(uninitializedVars);
+        this.taintedTemps = new HashSet<>(taintedTemps);
     }
     
     /****************************************/
@@ -51,22 +58,24 @@ public class UninitializedVarState implements DataflowState<UninitializedVarStat
     /**
      * Join this state with another (set union for "may" analysis).
      * If a variable is possibly uninitialized on ANY path, it's in the result.
+     * Same for tainted temporaries.
      */
     @Override
     public void join(UninitializedVarState other)
     {
         this.uninitializedVars.addAll(other.uninitializedVars);
+        this.taintedTemps.addAll(other.taintedTemps);
     }
-    
+
     /**
      * Create an independent copy of this state.
      */
     @Override
     public UninitializedVarState copy()
     {
-        return new UninitializedVarState(this.uninitializedVars);
+        return new UninitializedVarState(this.uninitializedVars, this.taintedTemps);
     }
-    
+
     /**
      * Check equality for fixed-point detection.
      */
@@ -76,13 +85,14 @@ public class UninitializedVarState implements DataflowState<UninitializedVarStat
         if (this == obj) return true;
         if (obj == null || getClass() != obj.getClass()) return false;
         UninitializedVarState other = (UninitializedVarState) obj;
-        return this.uninitializedVars.equals(other.uninitializedVars);
+        return this.uninitializedVars.equals(other.uninitializedVars)
+            && this.taintedTemps.equals(other.taintedTemps);
     }
-    
+
     @Override
     public int hashCode()
     {
-        return uninitializedVars.hashCode();
+        return 31 * uninitializedVars.hashCode() + taintedTemps.hashCode();
     }
     
     /****************************************/
@@ -112,11 +122,33 @@ public class UninitializedVarState implements DataflowState<UninitializedVarStat
     {
         return Collections.unmodifiableSet(uninitializedVars);
     }
-    
+
+    /****************************************/
+    /* TAINTED TEMP MANIPULATION METHODS   */
+    /****************************************/
+
+    /** Mark a temporary as tainted (holds uninitialized value) */
+    public void addTainted(Temp t)
+    {
+        taintedTemps.add(t.getSerialNumber());
+    }
+
+    /** Mark a temporary as clean (holds initialized value) */
+    public void removeTainted(Temp t)
+    {
+        taintedTemps.remove(t.getSerialNumber());
+    }
+
+    /** Check if a temporary is tainted */
+    public boolean isTainted(Temp t)
+    {
+        return taintedTemps.contains(t.getSerialNumber());
+    }
+
     @Override
     public String toString()
     {
-        return "UninitializedVarState{" + uninitializedVars + "}";
+        return "UninitializedVarState{vars=" + uninitializedVars + ", temps=" + taintedTemps + "}";
     }
 }
 
