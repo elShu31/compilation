316bf09002bb52ebc302c4ac51087f0efe8caa03
elshu31 <shufaru@mail.tau.ac.il>
2026-02-28 15:42:31 +0200

    ir: Fixed issue with global variables

--- ex5/src/ir/Ir.java
+++ ex5/src/ir/Ir.java
@@ -90,20 +90,56 @@ public class Ir {
 		// Output .data section strings first
 		MipsGenerator.getInstance().emitStrings(stringTable);
 
+		List<IrCommand> globalAllocations = new ArrayList<>();
+		List<IrCommand> globalInitializations = new ArrayList<>();
+		List<IrCommand> functionCommands = new ArrayList<>();
+
+		boolean inGlobalContext = true;
+
+		// Extract global allocations and initializations
+		for (int i = 0; i < commands.size(); i++) {
+			IrCommand cmd = commands.get(i);
+
+			if (inGlobalContext && cmd instanceof IrCommandLabel && (i + 1 < commands.size())
+					&& (commands.get(i + 1) instanceof IrCommandPrologue)) {
+				inGlobalContext = false;
+			}
+
+			if (inGlobalContext) {
+				if (cmd instanceof IrCommandAllocate) {
+					globalAllocations.add(cmd);
+				} else {
+					globalInitializations.add(cmd);
+				}
+			} else {
+				functionCommands.add(cmd);
+			}
+		}
+
+		// First, do global allocations
+		for (IrCommand cmd : globalAllocations) {
+			cmd.mipsMe();
+		}
+
 		List<IrCommand> currentFunc = new ArrayList<>();
 		boolean inFunc = false;
 
 		// Output commands inside .text
-		for (IrCommand cmd : commands) {
+		for (IrCommand cmd : functionCommands) {
 			if (cmd instanceof IrCommandPrologue) {
 				inFunc = true;
 				currentFunc.clear();
+				IrCommandPrologue pro = (IrCommandPrologue) cmd;
+				currentFunc.add(cmd);
+				if (pro.funcName.equals("main")) {
+					currentFunc.addAll(globalInitializations);
+				}
+				continue;
 			}
 
 			if (inFunc) {
 				currentFunc.add(cmd);
 			} else {
-				// Global allocations
 				cmd.mipsMe();
 			}
 
